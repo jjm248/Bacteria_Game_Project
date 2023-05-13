3c07980b3bf1641e44494e5e62f42e997334dddc
javasam1 <98443895+javasam1@users.noreply.github.com>
2023-05-13 18:38:08 +0900

    make framework

--- Bacteria _Game/main.cpp
+++ Bacteria _Game/main.cpp
@@ -1,43 +1,129 @@
-#include <iostream>
 #include <doodle/doodle.hpp>
-
-using namespace doodle;
+#include<iostream>
 using namespace std;
+using namespace doodle;
+int count_num = 1;
+class Stage1 {
+	int finish_count = 0;
+public:
+	void init() {
 
-double                   angle{ 0.0 };
-double                   grey{ 0 };
-double                   x{ 0.0 };
-double                   y{ 0.0 };
+	}
+	void logic() {
 
-void moved(int mouse_x, int mouse_y)
-{
-    angle += to_radians(3.0);
-    grey = 255 * (std::sin(angle) * 0.5 + 0.5);
-    x = mouse_x;
-    y = mouse_y;
-}
+	}
+	void draw() {
 
-int main(void)
-{
-    create_window(720, 720);
+	}
+	int end() {
+		return finish_count;
+	}
+};
+class Stage2 {
+	int finish_count = 0;
+public:
+	void init() {
 
-    const Image germ{ "./Game characters/germ_1.png" };
+	}
+	void logic() {
 
-    set_image_mode(RectMode::Center);
-    while (!is_window_closed())
-    {
-        update_window();
-        clear_background(255,255,255);
-        draw_image(germ, x, y, 100, 128);
-    }
+	}
+	void draw() {
 
-    set_rectangle_mode(RectMode::Center);
-    while (!is_window_closed())
-    {
-        update_window();
-        clear_background(220);
-        set_fill_color(grey);
-        draw_rectangle(x, y, Width * 0.4, Height * 0.4);
-    }
-    return 0;
+	}
+	int end() {
+		return finish_count;
+	}
+};
+class Stage3 {
+	int finish_count = 0;
+public:
+	void init() {
+
+	}
+	void logic() {
+
+	}
+	void draw() {
+
+	}
+	int end() {
+		return finish_count;
+	}
+};
+class Stage4 {
+	int finish_count = 0;
+public:
+	void init() {
+
+	}
+	void logic() {
+
+	}
+	void draw() {
+
+	}
+	int end() {
+		return finish_count;
+	}
+};
+class Stage5 {
+	int finish_count = 0;
+public:	
+	void init() {
+
+	}
+	void logic() {
+
+	}
+	void draw() {
+
+	}
+	int end() {
+		return finish_count;
+	}
+};
+int main() {
+	Stage1 stage1;
+	stage1.init();
+	Stage2 stage2;
+	stage2.init();
+	Stage3 stage3;
+	stage3.init();
+	Stage4 stage4;
+	stage4.init();
+	Stage5 stage5;
+	stage5.init();
+	create_window(720, 720);
+	while (!is_window_closed()) {
+		switch (count_num)
+		{
+		case 1:
+			stage1.logic();
+			stage1.draw();
+			count_num += stage1.end();
+			break;
+		case 2:
+			stage2.logic();
+			stage2.draw();
+			count_num += stage2.end();
+			break;
+		case 3:
+			stage3.logic();
+			stage3.draw();
+			count_num += stage3.end();
+			break;
+		case 4:
+			stage4.logic();
+			stage4.draw();
+			count_num += stage4.end();
+			break;
+		case 5:
+			stage5.logic();
+			stage5.draw();
+			count_num += stage5.end();
+			break;
+		}
+	}
+	
 }
